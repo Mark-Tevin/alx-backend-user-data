f617f293c26ddfff9ecebe95f52d345d070291a6
Mark-Tevin <tevinsmark@gmail.com>
2024-12-19 17:20:48 +0000

    User model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -2,7 +2,7 @@
 """
 This module defines a SQLAlchemy model for the 'users' database table.
 
-The `User` model maps attributes of a user to a database table 
+The `User` model maps attributes of a user to a database table
 using SQLAlchemy's ORM capabilities. With attributes such as `id`, `email`,
 `hashed_password`, `session_id`, and `reset_token`.
 """
@@ -24,8 +24,8 @@ class User(Base):
         __tablename__ (str): Table name in the db.
         id (Column): Primary key.
         email (Column): Non-nullable string storing user's email address.
-        hashed_password (Column): Non-nullable string storing the user's hashed password.
-        session_id (Column): Nullable string for user's session ID, for session management.
+        hashed_password : Non-nullable string storing hashed password.
+        session_id (Column): Nullable string for session management.
         reset_token (Column): Nullable string for the password reset token.
     """
     __tablename__ = 'users'
@@ -35,4 +35,3 @@ class User(Base):
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
-
